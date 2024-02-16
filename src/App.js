d7504738c86e84fa158922b47fe5235a756eb1ca
bsbhakti <70367592+bsbhakti@users.noreply.github.com>
2024-02-16 14:15:52 -0800

    Added login page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from "react";
+import Login from './components/LoginPage/LoginPage';
+import LandingTemp from "./components/LandingTemp";
+import { BrowserRouter as Router, Switch, Route,Routes, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/landingTemp" element={<LandingTemp />}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
