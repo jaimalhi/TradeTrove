7bde63b3f92f12c9320600d93425ddf596c763ed
Jai <malhi.jaivir@gmail.com>
2024-02-23 14:29:35 -0800

    init navbar using template, values to be changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
 // import './App.css';
+import "./tailwind.css";
+
 import React from "react";
 import Login from "./components/LoginPage/LoginPage";
 import LandingTemp from "./components/LandingTemp";
 import { BrowserRouter as Router, Switch, Route, Routes, Link } from "react-router-dom";
+import Navbar from "./components/Navbar/Narbar";
 
 function App() {
    return (
-      <Router>
-         <Routes>
-            <Route path="/" element={<Login />}></Route>
-            <Route path="/landingTemp" element={<LandingTemp />}></Route>
-         </Routes>
-      </Router>
+      <>
+         <Navbar />
+         <Router>
+            <Routes>
+               <Route path="/" element={<LandingTemp />}></Route>
+               <Route path="/login" element={<Login />}></Route>
+            </Routes>
+         </Router>
+      </>
    );
 }
 
