6521480f14db4b7b6ec9c97ebb7a40e76a88df55
bsbhakti <70367592+bsbhakti@users.noreply.github.com>
2024-03-20 14:41:32 -0700

    Converting base64 string to binary before adding it to db

--- frontend/src/components/CustomerServices/ImageViewer.js
+++ frontend/src/components/CustomerServices/ImageViewer.js
@@ -5,11 +5,9 @@ function ImageViewer({ apiData }) {
 
    useEffect(() => {
       if (!apiData || !apiData.imageData) return;
-      const arrayBuffer = apiData.imageData.data;
-      console.log(arrayBuffer);
-
-      const base64String = btoa(String.fromCharCode(...arrayBuffer));
-      setImageSrc(`data:image/png;base64,${base64String}`);
+      const arrayBuffer = apiData.imageData;
+      
+      setImageSrc(`data:image/png;base64,${arrayBuffer}`);
    }, [apiData]);
 
    return <div>{imageSrc && <img src={imageSrc} alt={apiData.imageTitle} className="mt-2" />}</div>;
