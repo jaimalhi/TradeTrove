110702fe3905794b1bf8e6620d3535264ca11fe0
bsbhakti <70367592+bsbhakti@users.noreply.github.com>
2024-03-20 21:36:27 -0700

    Build folder

--- frontend/src/utils/apiHelper.js
+++ frontend/src/utils/apiHelper.js
@@ -1,4 +1,4 @@
-const baseURL = "http://localhost:8080";
-// const baseURL = "https://cmpt-372-411622.uc.r.appspot.com";
+// const baseURL = "http://localhost:8080";
+const baseURL = "https://cmpt-372-411622.uc.r.appspot.com";
 
 module.exports = { baseURL };
