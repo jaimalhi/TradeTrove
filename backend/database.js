c70fec60a5f88579b6140952ca423403be90a516
Jai <malhi.jaivir@gmail.com>
2024-03-20 21:02:13 -0700

    removed db secret info

--- backend/database.js
+++ backend/database.js
@@ -7,10 +7,6 @@ const pool = new Pool({
    host: process.env.DB_HOST,
    database: process.env.DB_NAME,
    password: process.env.DB_PASS,
-   //    user: "postgres",
-   //    host: "34.170.231.213",
-   //    database: "postgres",
-   //    password: "admin",
    port: 5432, // Default port for PostgreSQL
    ssl: {
       rejectUnauthorized: false,
