d0227a3943ec728412bf8623d4203f5ffebd08bf
Jagdeep <jsa397@sfu.ca>
2024-03-25 10:39:57 -0700

    added the initials name card and some styling

--- frontend/src/components/ViewTradies/ViewTradies.js
+++ frontend/src/components/ViewTradies/ViewTradies.js
@@ -3,58 +3,65 @@ import axios from "axios";
 import { baseURL } from "../../utils/apiHelper";
 
 function ViewTradies() {
-   const [tradies, setTradies] = useState([]);
+    const [tradies, setTradies] = useState([]);
 
-   useEffect(() => {
-      const fetchTradies = async () => {
-         try {
-            const response = await axios.get(`${baseURL}/api/tradies/`, {
-               withCredentials: true,
-            });
-            setTradies(response.data);
-         } catch (error) {
-            console.error("Error fetching tradies:", error);
-         }
-      };
+    useEffect(() => {
+        const fetchTradies = async () => {
+            try {
+                const response = await axios.get(`${baseURL}/api/tradies/`, {
+                    withCredentials: true,
+                });
+                setTradies(response.data);
+            } catch (error) {
+                console.error("Error fetching tradies:", error);
+            }
+        };
 
-      fetchTradies();
-   }, []);
+        fetchTradies();
+    }, []);
 
-   console.log(tradies);
+    // Function to get initials from first and last name
+    const getInitials = (firstName, lastName) => {
+        return `${firstName[0]}${lastName[0]}`;
+    };
 
-   return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-white px-4 py-8 pt-[100px]">
-         <h2 className="text-center text-dark-green mb-10 text-3xl font-bold">
-            List of Contractors
-         </h2>
-         <div className="w-full max-w-2xl">
-            {tradies.length > 0 ? (
-               tradies.map((tradie) => (
-                  <div key={tradie.uid} className="bg-white p-4 rounded-lg shadow-md mb-6">
-                     <h3 className="text-xl font-semibold mb-2">
-                        {tradie.first_name} {tradie.last_name}
-                     </h3>
-                     <p className="mb-1">
-                        Email: <span className="font-medium">{tradie.email}</span>
-                     </p>
-                     <p className="mb-1">
-                        Phone: <span className="font-medium">{tradie.phone_num}</span>
-                     </p>
-                     <p className="mb-1">
-                        Experience:{" "}
-                        <span className="font-medium">{tradie.years_experience} years</span>
-                     </p>
-                     <p>
-                        Skills: <span className="font-medium">{tradie.skills.join(", ")}</span>
-                     </p>
-                  </div>
-               ))
-            ) : (
-               <p className="text-lg">No tradies found.</p>
-            )}
-         </div>
-      </div>
-   );
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-white px-4 py-8 pt-[100px]">
+            <h2 className="text-center text-dark-green mb-10 text-3xl font-bold">
+                List of Contractors
+            </h2>
+            <div className="w-full max-w-2xl ">
+                {tradies.length > 0 ? (
+                    tradies.map((tradie) => (
+                        <div key={tradie.uid} className="bg-white p-4 rounded-lg border border-medium-green shadow-md mb-6 flex items-center">
+                            <div className="flex-none mr-10 flex items-center justify-center h-24 w-24 rounded-full bg-light-green text-white text-3xl">
+                                {getInitials(tradie.first_name, tradie.last_name)}
+                            </div>
+                            <div className="flex-grow ">
+                                <h3 className="text-xl font-semibold mb-2">
+                                    {tradie.first_name} {tradie.last_name}
+                                </h3>
+                                <p className="mb-1">
+                                    Email: <span className="font-medium">{tradie.email}</span>
+                                </p>
+                                <p className="mb-1">
+                                    Phone: <span className="font-medium">{tradie.phone_num}</span>
+                                </p>
+                                <p className="mb-1">
+                                    Experience: <span className="font-medium">{tradie.years_experience} years</span>
+                                </p>
+                                <p>
+                                    Skills: <span className="font-medium">{tradie.skills.join(", ")}</span>
+                                </p>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p className="text-lg">No tradies found.</p>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default ViewTradies;
