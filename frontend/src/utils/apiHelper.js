cc353604a9236365d509e5f260818b942ea4a389
Jai <malhi.jaivir@gmail.com>
2024-04-04 15:23:42 -0700

    change baseUrl to localhost

--- frontend/src/utils/apiHelper.js
+++ frontend/src/utils/apiHelper.js
@@ -1,5 +1,5 @@
-// const baseURL = "http://localhost:8080";
+const baseURL = "http://localhost:8080";
 // const baseURL = "https://cmpt-372-411622.uc.r.appspot.com";
-const baseURL = "http://34.121.253.218:8080";
+// const baseURL = "http://34.30.64.75:8080";
 
 module.exports = { baseURL };
