77b4ba25fbfa685d3983f72914f59d9b0d33ae1e
Jai <malhi.jaivir@gmail.com>
2024-04-06 15:28:19 -0700

    fixed tradies not being able to view customer job images

--- frontend/src/utils/ImageViewer.js
+++ frontend/src/utils/ImageViewer.js
@@ -6,11 +6,19 @@ function ImageViewer({ apiData }) {
    useEffect(() => {
       if (!apiData || !apiData.imageData) return;
       const arrayBuffer = apiData.imageData;
-      
+
       setImageSrc(`data:image/png;base64,${arrayBuffer}`);
    }, [apiData]);
 
-   return <div>{imageSrc && <img src={imageSrc} alt={apiData.imageTitle} className="mt-2" />}</div>;
+   //    return <div>{imageSrc && <img src={imageSrc} alt={apiData.imageTitle} className="mt-2" />}</div>;
+
+   return (
+      <div>
+         {imageSrc && (
+            <img src={imageSrc} alt={apiData.imageTitle} className="w-60 h-60 rounded-xl" />
+         )}
+      </div>
+   );
 }
 
 export default ImageViewer;
